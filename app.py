218c5124b5e71c2464e2870709e7cc71251b82c2
infinity1009 <68230908+infinity1009@users.noreply.github.com>
2022-05-04 21:32:16 +0800

    v1

---
+++ app.py
@@ -0,0 +1,20 @@
+from datetime import timedelta
+from flask import Flask, redirect
+import log_manage, student, teacher, admin
+
+app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
+app.register_blueprint(log_manage.bp)
+app.register_blueprint(student.bp)
+app.register_blueprint(teacher.bp)
+app.register_blueprint(admin.bp)
+
+
+@app.route('/')
+def homepage():  # 主页展示
+    return redirect('/login')
+
+
+if __name__ == '__main__':
+    app.run()
