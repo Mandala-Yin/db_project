8d59751f02f70544484cf7ef7eae1ac2fe1fcbe0
2367752205@qq.com <52498812+Orange1918@users.noreply.github.com>
2022-05-15 15:50:12 +0800

    modify scores, simple version

--- student.py
+++ student.py
@@ -9,7 +9,7 @@ def student(sid):
     basic_info = fetch_basic_information(sid, 'student')
     if session.get('user_id') is not None:
         # 正常情况下，应该进入展示学生基本信息的页面student_info.html
-        return render_template('student_info.html', basic_info=basic_info)
+        return render_template('student/student_info.html', basic_info=basic_info)
     else:
         return redirect(url_for('log_manage.login'))
 
@@ -18,7 +18,7 @@ def student(sid):
 def student_edit(sid):
     # 通过主页中的修改按钮可以进入该函数
     basic_info = fetch_basic_information(sid, 'student')  # 传基本信息的原因是，可以在修改页面预填充原信息，然后可以仅修改某几项信息
-    return render_template('stu_edit.html', basic_info=basic_info)
+    return render_template('student/stu_edit.html', basic_info=basic_info)
 
 
 @bp.route('/update', methods=['POST'])
@@ -44,4 +44,4 @@ def course_check(sid):
     basic_info = fetch_basic_information(sid, 'student') # 基本信息，方便回到学生主页
     results_major = fetch_stu_course(sid)  # 主学位课程信息
     results_non_major = fetch_stu_course(sid, False)  # 双学位课程信息，注意results可能为空
-    return render_template('stu_course.html', major=results_major, minor=results_non_major, basic_info=basic_info)
+    return render_template('student/stu_course.html', major=results_major, minor=results_non_major, basic_info=basic_info)
--- teacher.py
+++ teacher.py
@@ -5,7 +5,6 @@ from db_func import fetch_basic_information, update_user_info, fetch_tea_course,
 
 bp = Blueprint('teacher', __name__, url_prefix='/teacher')
 
-
 @bp.route('/<string:tid>', methods=['GET', 'POST'])
 def teacher(tid):
     basic_info = fetch_basic_information(tid, 'teacher')
@@ -46,39 +45,28 @@ def course_info(tid):
     results_course = fetch_tea_course(tid)
     return render_template('teacher/tea_course.html', courses=results_course, basic_info=basic_info)
 
-@bp.route('/choose_course/add/<string:tid>')
-def choose_course_for_add(tid):
+@bp.route('/choose_course/<string:tid>')
+def choose_course(tid):
     basic_info = fetch_basic_information(tid, 'teacher')
     results_course = fetch_tea_course(tid)
     # 点击添加分数/修改分数按钮后进入该函数
-    return render_template('teacher/input_add_scores.html', courses=results_course, basic_info=basic_info)
-
-@bp.route('/choose_course/add/<string:tid>')
-def choose_course_for_modify(tid):
-    # 点击修改分数按钮后进入该函数
-    basic_info = fetch_basic_information(tid, 'teacher')
-    results_course = fetch_tea_course(tid)
-    return render_template('teacher/input_modify_scores.html', courses=results_course, basic_info=basic_info)
+    return render_template('teacher/input_course_info.html', courses=results_course, basic_info=basic_info)
 
 
 @bp.route('/add_scores/<string:tid>', methods=['POST'])
-def add_scores_view(tid):
-    """
-    点击录入成绩按钮后进入该函数（之前要输入课程号和对应学期），然后转入对应的html页面
-    我的想法是，录入和修改成绩的修改在于，录入成绩时不显示SC表中的score（理论上也应该是null）
-    修改成绩时预填充SC表中的score，然后可以有选择的进行修改
-    """
+def modify_scores_view(tid):
     _cid = request.form['cid']
     _semester = request.form['semester']
+    basic_info = fetch_basic_information(tid, 'teacher')
     # 确认输入了课程号和学期
     if _cid and _semester:
         # 预查询相关信息，然后转入html页面
-        results = fetch_course_without_score(_cid, tid, _semester)
-        return render_template('teacher/tea_add_scores.html', tid=tid, cid=_cid, sem=_semester, res=results)
+        results = fetch_course_with_score(_cid, tid, _semester)
+        return render_template('teacher/tea_modify_scores.html', basic_info=basic_info, cid=_cid, sem=_semester, res=results)
     else:
         return redirect(url_for('teacher.teacher', tid=tid))
 
-
+'''
 # 第一种路由来自教师主页点击修改课程分数信息按钮，第二种路由来自修改分数后
 @bp.route('/modify_scores/<string:tid>', methods=['POST'])
 @bp.route('/modify_scores/<string:tid>/<string:cid>/<string:semester>')
@@ -100,19 +88,18 @@ def modify_scores_view(tid, cid=None, semester=None):
             return render_template('teacher/tea_modify_scores.html', tid=tid, cid=cid, sem=semester, res=results)
         else:
             return redirect(url_for('teacher.teacher', tid=tid))
+'''
 
-
-@bp.route('/add_scores_confirm/<string:tid>/<string:cid>', methods=['POST'])
-def add_scores_confirm(tid, cid):
+@bp.route('/modify_scores_confirm/<string:tid>/<string:cid>', methods=['POST'])
+def modify_scores_confirm(tid, cid):
     # 点击录入成绩页面的提交按钮进入该函数，这里假设页面表单设置为list类型，即sids, scores都是list的格式
     sids = request.form.getlist('sids')
     scores = request.form.getlist('scores')
     update_scores(sids, scores, cid)
-    flash("add successfully!")
     # 完全录入后回到教师主页
     return redirect(url_for('teacher.teacher', tid=tid))
 
-
+'''
 @bp.route('/modify_scores_confirm/<string:tid>/<string:cid>/<string:semester>', methods=['POST'])
 def modify_scores_confirm(tid, cid, semester):
     # 点击修改成绩页面的提交按钮进入该函数，每条记录应该都有一个提交按钮，所以是分条更新
@@ -122,3 +109,5 @@ def modify_scores_confirm(tid, cid, semester):
     flash("modify successfully!")
     # 因为可能修改多条成绩，所以修改一条之后依然回到'modify_scores_view'
     return redirect(url_for('teacher.modify_scores_view', tid=tid, cid=cid, semester=semester))
+
+'''
