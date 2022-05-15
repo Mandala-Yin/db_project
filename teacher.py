a76856a6906687760d3e887db597b96cdfc82ae9
2367752205@qq.com <52498812+Orange1918@users.noreply.github.com>
2022-05-15 13:54:04 +0800

    jump logic for add & modify scores

--- teacher.py
+++ teacher.py
@@ -1,3 +1,4 @@
+from operator import mod
 from flask import Blueprint, redirect, render_template, request, session, url_for, flash
 from db_func import fetch_basic_information, update_user_info, fetch_tea_course, \
     fetch_course_with_score, fetch_course_without_score, update_scores
@@ -10,7 +11,7 @@ def teacher(tid):
     basic_info = fetch_basic_information(tid, 'teacher')
     if session.get('user_id') is not None:
         # 正常的话，应该进入展示教师基本信息的页面teacher_info.html
-        return render_template('teacher_info.html', basic_info=basic_info)
+        return render_template('teacher/teacher_info.html', basic_info=basic_info)
     else:
         return redirect(url_for('log_manage.login'))
 
@@ -19,7 +20,7 @@ def teacher(tid):
 def teacher_edit(tid):
     # 通过主页中的修改按钮可以进入该函数
     basic_info = fetch_basic_information(tid, 'teacher')  # 传基本信息的原因是，可以在修改页面预填充原信息，然后可以仅修改某几项信息
-    return render_template('tea_edit.html', basic_info=basic_info)
+    return render_template('teacher/tea_edit.html', basic_info=basic_info)
 
 
 @bp.route('/update', methods=['POST'])
@@ -43,7 +44,21 @@ def course_info(tid):
     # 点击查看开设课程按钮后进入该函数
     basic_info = fetch_basic_information(tid, 'teacher')
     results_course = fetch_tea_course(tid)
-    return render_template('tea_course.html', courses=results_course, basic_info=basic_info)
+    return render_template('teacher/tea_course.html', courses=results_course, basic_info=basic_info)
+
+@bp.route('/choose_course/add/<string:tid>')
+def choose_course_for_add(tid):
+    basic_info = fetch_basic_information(tid, 'teacher')
+    results_course = fetch_tea_course(tid)
+    # 点击添加分数/修改分数按钮后进入该函数
+    return render_template('teacher/input_add_scores.html', courses=results_course, basic_info=basic_info)
+
+@bp.route('/choose_course/add/<string:tid>')
+def choose_course_for_modify(tid):
+    # 点击修改分数按钮后进入该函数
+    basic_info = fetch_basic_information(tid, 'teacher')
+    results_course = fetch_tea_course(tid)
+    return render_template('teacher/input_modify_scores.html', courses=results_course, basic_info=basic_info)
 
 
 @bp.route('/add_scores/<string:tid>', methods=['POST'])
@@ -59,7 +74,7 @@ def add_scores_view(tid):
     if _cid and _semester:
         # 预查询相关信息，然后转入html页面
         results = fetch_course_without_score(_cid, tid, _semester)
-        return render_template('tea_add_scores.html', tid=tid, cid=_cid, sem=_semester, res=results)
+        return render_template('teacher/tea_add_scores.html', tid=tid, cid=_cid, sem=_semester, res=results)
     else:
         return redirect(url_for('teacher.teacher', tid=tid))
 
@@ -76,13 +91,13 @@ def modify_scores_view(tid, cid=None, semester=None):
         if _cid and _semester:
             # 预查询相关信息，然后转入html页面
             results = fetch_course_with_score(_cid, tid, _semester)
-            return render_template('tea_modify_scores.html', tid=tid, cid=_cid, sem=_semester, res=results)
+            return render_template('teacher/tea_modify_scores.html', tid=tid, cid=_cid, sem=_semester, res=results)
         else:
             return redirect(url_for('teacher.teacher', tid=tid))
     else:
         if cid and semester:
             results = fetch_course_with_score(cid, tid, semester)
-            return render_template('modify_scores.html', tid=tid, cid=cid, sem=semester, res=results)
+            return render_template('teacher/tea_modify_scores.html', tid=tid, cid=cid, sem=semester, res=results)
         else:
             return redirect(url_for('teacher.teacher', tid=tid))
 
