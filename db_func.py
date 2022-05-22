28899ab8f6bdb5f75b7e20a89bc9377af8fda706
infinity1009 <68230908+infinity1009@users.noreply.github.com>
2022-05-22 16:29:14 +0800

    change string concatenation form sql queries to parameterized sql queries.

--- db_func.py
+++ db_func.py
@@ -77,24 +77,25 @@ def login_check(u_id, u_password, u_role):
 def fetch_basic_information(u_id, u_role):
     db = get_db_connection()
     cur = db.cursor()
+    paras = [u_id]
     if u_role == 'admin':
         cur.execute(f'SELECT id, name, sex, fname, telephone, address, working_year, is_cadre '
                     f'FROM "admin_information" '
-                    f'WHERE "admin_information".id=\'{u_id}\'')
+                    f'WHERE "admin_information".id=%s', paras)
         raw_res = cur.fetchone()
         close_db_connection(cur, db)
         return format_trans_one(raw_res, default_map+adm_extra)
     elif u_role == 'teacher':
         cur.execute(f'SELECT id, name, sex, fname, telephone, address, working_year, title '
                     f'FROM "teacher_information" '
-                    f'WHERE "teacher_information".id=\'{u_id}\'')
+                    f'WHERE "teacher_information".id=%s', paras)
         raw_res = cur.fetchone()
         close_db_connection(cur, db)
         return format_trans_one(raw_res, default_map + tea_extra)
     else:
         cur.execute(f'SELECT id, name, sex, fname, telephone, address, grade, is_foreign_stu '
                     f'FROM "student_information" '
-                    f'WHERE "student_information".id=\'{u_id}\'')
+                    f'WHERE "student_information".id=%s', paras)
         raw_res = cur.fetchone()
         close_db_connection(cur, db)
         return format_trans_one(raw_res, default_map + stu_extra)
@@ -103,10 +104,11 @@ def fetch_basic_information(u_id, u_role):
 # 更改用户信息
 def update_user_info(_id, _telephone, _address):
     # 调用存储过程: 'modify_users'
-    sql = f'CALL modify_users(\'{_telephone}\', \'{_address}\', \'{_id}\');'
+    sql = f'CALL modify_users(%s, %s, %s);'
+    paras = [_telephone, _address, _id]
     db = get_db_connection()
     cur = db.cursor()
-    cur.execute(sql)
+    cur.execute(sql, paras)
     db.commit()
     close_db_connection(cur, db)
 
@@ -118,11 +120,12 @@ def fetch_stu_course(sid, is_major=True):
           f'SELECT "SC".cid, "SC".score, CAST((4-3*(100-"SC".score)^2/1600) as DECIMAL(3,2)) AS gpa, "SC".is_w, ' \
           f'"SC".semester, "Course".cname, "Course".category, "Course".credit ' \
           f'FROM "SC", "Course" ' \
-          f'WHERE "SC".sid = \'{sid}\' and "SC".is_major={is_major} ' \
+          f'WHERE "SC".sid = %s and "SC".is_major= %s ' \
           f'and "SC".cid = "Course".cid and "SC".tid = "Course".tid'
+    paras = [sid, is_major]
     db = get_db_connection()
     cur = db.cursor()
-    cur.execute(sql)
+    cur.execute(sql, paras)
     db.commit()
     sql = f'SELECT * FROM student_course'
     cur.execute(sql)
@@ -141,10 +144,10 @@ def fetch_stu_course(sid, is_major=True):
 
 # 获取去重的课程号
 def fetch_cid(tid):
-    sql = f'SELECT "Course".cid UNIQUE FROM "Course" WHERE "Course".tid=\'{tid}\''
+    sql = f'SELECT "Course".cid UNIQUE FROM "Course" WHERE "Course".tid=%s'
     db = get_db_connection()
     cur = db.cursor()
-    cur.execute(sql)
+    cur.execute(sql, [tid])
     results = format_trans(cur.fetchall(), ['cid'])
     close_db_connection(cur, db)
 
@@ -156,16 +159,17 @@ def fetch_tea_course(tid):
     # 按课程、学期统计平均分、最高分 建立了临时视图
     sql = f'WITH "S_ANY"(cid, semester, avg_score, max_score) AS ( ' \
           f'SELECT cid, semester, CAST(AVG("SC".score) as DECIMAL(5,2)), MAX("SC".score) ' \
-          f'FROM "SC" WHERE "SC".tid=\'{tid}\' GROUP BY "SC".cid, "SC".semester) ' \
+          f'FROM "SC" WHERE "SC".tid=%s GROUP BY "SC".cid, "SC".semester) ' \
           f'SELECT "Course".cid, "Course".cname, "Course".category, "Course".credit, ' \
           f'"S_ANY".semester, "S_ANY".avg_score, "S_ANY".max_score ' \
-          f'FROM "Course", "S_ANY" WHERE "Course".cid="S_ANY".cid and "Course".tid=\'{tid}\' ' \
+          f'FROM "Course", "S_ANY" WHERE "Course".cid="S_ANY".cid and "Course".tid=%s ' \
           f'UNION ' \
           f'SELECT "Course".cid, "Course".cname, "Course".category, "Course".credit, null, null, null ' \
-          f'FROM "Course" WHERE "Course".cid not in (SELECT cid FROM "SC") AND "Course".tid=\'{tid}\''
+          f'FROM "Course" WHERE "Course".cid not in (SELECT cid FROM "SC") AND "Course".tid=%s'
+    paras = [tid] * 3
     db = get_db_connection()
     cur = db.cursor()
-    cur.execute(sql)
+    cur.execute(sql, paras)
     results = format_trans(cur.fetchall(), TC_info)
     close_db_connection(cur, db)
 
@@ -177,11 +181,12 @@ def fetch_course_without_score(cid, tid, semester):
     # 已经中期退课的学生成绩不由老师负责
     sql = f'SELECT "SC".sid, "student_information".name ' \
           f'FROM "SC", "student_information" ' \
-          f'WHERE "SC".cid=\'{cid}\' and "SC".tid=\'{tid}\' and "SC".semester=\'{semester}\' and ' \
+          f'WHERE "SC".cid=%s and "SC".tid=%s and "SC".semester=%s and ' \
           f'"SC".sid="student_information".id and "SC".is_w=False'
+    paras = [cid, tid, semester]
     db = get_db_connection()
     cur = db.cursor()
-    cur.execute(sql)
+    cur.execute(sql, paras)
     results = format_trans(cur.fetchall(), ['sid', 's_name'])
     close_db_connection(cur, db)
 
@@ -193,11 +198,12 @@ def fetch_course_with_score(cid, tid, semester):
     # 已经中期退课的学生成绩不由老师负责
     sql = f'SELECT "SC".sid, "student_information".name, "SC".score ' \
           f'FROM "SC", "student_information" ' \
-          f'WHERE "SC".cid=\'{cid}\' and "SC".tid=\'{tid}\' and "SC".semester=\'{semester}\' and ' \
+          f'WHERE "SC".cid=%s and "SC".tid=%s and "SC".semester=%s and ' \
           f'"SC".sid="student_information".id and "SC".is_w=False'
+    paras = [cid, tid, semester]
     db = get_db_connection()
     cur = db.cursor()
-    cur.execute(sql)
+    cur.execute(sql, paras)
     results = format_trans(cur.fetchall(), ['sid', 's_name', 'score'])
     close_db_connection(cur, db)
 
@@ -209,8 +215,9 @@ def update_scores(sids, scores, cid):
     db = get_db_connection()
     cur = db.cursor()
     for sid, score in zip(sids, scores):
-        sql = f'UPDATE "SC" SET score=\'{score}\' WHERE sid=\'{sid}\' and cid=\'{cid}\''
-        cur.execute(sql)
+        sql = f'UPDATE "SC" SET score=%s WHERE sid=%s and cid=%s'
+        paras = [score, sid, cid]
+        cur.execute(sql, paras)
     db.commit()
     close_db_connection(cur, db)
 
@@ -219,8 +226,8 @@ def update_scores(sids, scores, cid):
 def faculty_id(aid):
     db = get_db_connection()
     cur = db.cursor()
-    sql = f'SELECT fid FROM "Users" WHERE id=\'{aid}\''
-    cur.execute(sql)
+    sql = f'SELECT fid FROM "Users" WHERE id=%s'
+    cur.execute(sql, [aid])
     result = format_trans_one(cur.fetchone(), ['fid'])
     close_db_connection(cur, db)
 
@@ -232,14 +239,15 @@ def faculty2stu(fid):
     db = get_db_connection()
     cur = db.cursor()
     # 按性别统计人数
-    sql = f'SELECT sex, COUNT(*) FROM student_information WHERE fid=\'{fid}\' ' \
+    sql = f'SELECT sex, COUNT(*) FROM student_information WHERE fid=%s ' \
           f'GROUP BY sex'
-    cur.execute(sql)
+    paras = [fid]
+    cur.execute(sql, paras)
     res_sex = format_trans(cur.fetchall(), ['sex', 'stu nums'])
     # 按年级统计人数
-    sql = f'SELECT grade, COUNT(*) FROM student_information WHERE fid=\'{fid}\' ' \
+    sql = f'SELECT grade, COUNT(*) FROM student_information WHERE fid=%s ' \
           f'GROUP BY grade'
-    cur.execute(sql)
+    cur.execute(sql, paras)
     res_grade = format_trans(cur.fetchall(), ['grade', 'stu nums'])
     close_db_connection(cur, db)
 
@@ -251,14 +259,15 @@ def faculty2tea(fid):
     db = get_db_connection()
     cur = db.cursor()
     # 按性别统计人数
-    sql = f'SELECT sex, COUNT(*) FROM teacher_information WHERE fid=\'{fid}\' ' \
+    sql = f'SELECT sex, COUNT(*) FROM teacher_information WHERE fid=%s ' \
           f'GROUP BY sex'
-    cur.execute(sql)
+    paras = [fid]
+    cur.execute(sql, paras)
     res_sex = format_trans(cur.fetchall(), ['sex', 'tea nums'])
     # 按职称统计人数
-    sql = f'SELECT title, COUNT(*) FROM teacher_information WHERE fid=\'{fid}\' ' \
+    sql = f'SELECT title, COUNT(*) FROM teacher_information WHERE fid=%s ' \
           f'GROUP BY title'
-    cur.execute(sql)
+    cur.execute(sql, paras)
     res_title = format_trans(cur.fetchall(), ['title', 'tea nums'])
     close_db_connection(cur, db)
 
@@ -271,9 +280,9 @@ def faculty2course(fid):
     cur = db.cursor()
     # 按类别统计课程数，这里在用COUNT聚集函数的时候加了distinct关键字，因为Course表中同一课程可能有多名教师授课，但是只算一次
     sql = f'SELECT category, COUNT(distinct cid) FROM "Course" WHERE tid in ' \
-          f'(SELECT id FROM teacher_information WHERE fid=\'{fid}\') ' \
+          f'(SELECT id FROM teacher_information WHERE fid=%s) ' \
           f'GROUP BY category'
-    cur.execute(sql)
+    cur.execute(sql, [fid])
     res_cate = format_trans(cur.fetchall(), ['category', 'course nums'])
     close_db_connection(cur, db)
 
@@ -284,8 +293,8 @@ def faculty2course(fid):
 def get_tid(fid):
     db = get_db_connection()
     cur = db.cursor()
-    sql = f'SELECT id FROM teacher_information WHERE fid=\'{fid}\''
-    cur.execute(sql)
+    sql = f'SELECT id FROM teacher_information WHERE fid=%s'
+    cur.execute(sql, [fid])
     res_tid = format_trans(cur.fetchall(), ['tid'])
     close_db_connection(cur, db)
 
@@ -298,7 +307,7 @@ def get_teachers_by_fid(fid):
     cur = db.cursor()
     cur.execute(f'SELECT id, name, sex, fname, telephone, address, working_year, title '
                 f'FROM "teacher_information" '
-                f'WHERE "teacher_information".fid=\'{fid}\'')
+                f'WHERE "teacher_information".fid=%s', [fid])
     res = format_trans(cur.fetchall(), default_map + tea_extra)
     close_db_connection(cur, db)
     return res
@@ -310,7 +319,7 @@ def get_students_by_fid(fid):
     cur = db.cursor()
     cur.execute(f'SELECT id, name, sex, fname, telephone, address, grade, is_foreign_stu '
                 f'FROM "student_information" '
-                f'WHERE "student_information".fid=\'{fid}\'')
+                f'WHERE "student_information".fid=%s', [fid])
     res = format_trans(cur.fetchall(), default_map + stu_extra)
     close_db_connection(cur, db)
     return res
@@ -320,7 +329,8 @@ def get_students_by_fid(fid):
 def update_course(cid, tid, cname, category, credit):
     db = get_db_connection()
     cur = db.cursor()
-    sql = f'INSERT INTO "Course" VALUES (\'{cid}\', \'{tid}\', \'{cname}\', \'{category}\', {credit})'
-    cur.execute(sql)
+    sql = f'INSERT INTO "Course" VALUES (%s, %s, %s, %s, %s)'
+    paras = [cid, tid, cname, category, credit]
+    cur.execute(sql, paras)
     db.commit()
     close_db_connection(cur, db)
